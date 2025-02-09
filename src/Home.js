5e586ccbbe47c992ee9f7e9c9cf6c72497f9fd70
zlkca <likzhang@gmail.com>
2025-02-09 15:23:07 -0500

    feat v1

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 import "./Home.css"
+
 export const Home = () => {
     return <div>
         <Header />
@@ -11,8 +12,25 @@ export const Home = () => {
                     Streamline operations, enhance communication, and provide better care with our comprehensive school management software
                 </p>
                 <a href="https://admin.podtree.ca/signup">
-                <button className="hero-button">Try the Demo</button>
+                    <button className="hero-button">Start for Free</button>
                 </a>
+                <p className="hero-note">Free to use for 6 months</p>
+                <p className="hero-note">No credit card required | Get started quickly</p>
+            </div>
+        </div>
+        <div className="features">
+            <h2 className="features-title">Key Features</h2>
+            <div className="feature">
+                <h3 className="feature-title">Enrollment Management</h3>
+                <p className="feature-description">Easy registration & tracking.</p>
+            </div>
+            <div className="feature">
+                <h3 className="feature-title">Invoice Management</h3>
+                <p className="feature-description">Automated invoicing, online payments</p>
+            </div>
+            <div className="feature">
+                <h3 className="feature-title">Attendance Tracking</h3>
+                <p className="feature-description">Daily check-ins/check-outs.</p>
             </div>
         </div>
     </div>
